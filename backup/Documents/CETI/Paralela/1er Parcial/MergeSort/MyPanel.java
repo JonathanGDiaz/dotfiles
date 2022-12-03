878b34425a321ee1d50ac63389f361103ccaa349
JonathanGDiaz <morokeidovah@gmail.com>
2022-12-03 12:19:07 -0600

    removing of some files not needed in back up

--- backup/Documents/CETI/Paralela/1er Parcial/MergeSort/MyPanel.java
+++
@@ -1,6 +0,0 @@
-/**
- * MyPanel
- */
-public class MyPanel {
-
-}
--- backup/Documents/Cursos/C++/Holamundo/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-int main(int argc, char *argv[]) {
-  std::cout << "hola mundo!" << std::endl;
-  return 0;
-}
